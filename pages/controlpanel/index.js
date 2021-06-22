53cca9581d800b39b013266b079f115a565d2cc0
qt-coder <chuchenkhang@gmail.com>
2021-06-22 08:22:10 +0800

    Protected control panel

--- pages/controlpanel/index.js
+++ pages/controlpanel/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import CardContainer from '../../components/CardContainer'
 import Head from 'next/head'
+import { useSession } from 'next-auth/client'
 
 function index() {
+
+    const [session, loading] = useSession()
+
     return (
         <>
-            <Head>
-                <title>Cath Control Panel</title>
-            </Head>
-            <div>
-                <h1>Control Panel</h1>
-                <CardContainer link='/controlpanel/commands'>Commands</CardContainer>
-            </div>
+            {!session && <>
+                <h1>Seems like you&apos;re not logged in. Log in to get started!</h1>
+            </>
+            }
+            {session && <>
+                <Head>
+                    <title>Cath Control Panel</title>
+                </Head>
+                <div>
+                    <h1>Control Panel</h1>
+                    <CardContainer link='/controlpanel/commands'>Commands</CardContainer>
+                </div>
+            </>
+            }
         </>
     )
 }
