6e2722c8ab0aa9cd24503b5f7e74c25c10091978
qt coder <chuchenkhang@gmail.com>
2021-06-27 18:44:22 +0800

    Changed Navbar to use Chakra UI

--- components/AuthButton.js
+++ components/AuthButton.js
@@ -1,15 +1,39 @@
 import { signIn, signOut, useSession } from 'next-auth/client'
-import { Button } from '@material-ui/core'
+import {Button} from '@chakra-ui/react' 
 
 export default function Page() {
     const [session, loading] = useSession()
 
     return <>
         {!session && <>
-            <Button onClick={() => signIn()}>Sign in</Button>
+            <Button
+                display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={'sm'}
+                fontWeight={600}
+                color={'white'}
+                bg={'pink.400'}
+                href={'#'}
+                _hover={{
+                bg: 'pink.300',
+                }}
+                onClick={()=>signIn()}>
+                Sign In
+            </Button>
         </>}
         {session && <>
-            <Button onClick={() => signOut()}>Sign out</Button>
+            <Button
+                display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={'sm'}
+                fontWeight={600}
+                color={'white'}
+                bg={'pink.400'}
+                href={'#'}
+                _hover={{
+                    bg: 'pink.300',
+                }}
+                onClick={() => signOut()}>
+                Sign Out
+            </Button>
         </>}
     </>
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,24 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import Head from 'next/head'
 import { Provider } from 'next-auth/client'
+import { ChakraProvider } from "@chakra-ui/react"
+import Header from '../components/Nav/Header'
+import theme from '../chakra_config/theme'
+import { ColorModeScript } from "@chakra-ui/react"
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider>
-      <Head>
-        <title>Cath Dashboard</title>
-      </Head>
-      <Navbar/>
-      <div className='page-container'>
-        <Component {...pageProps} />
-      </div>
+      <ChakraProvider>
+        <Head>
+          <title>Cath Dashboard</title>
+        </Head>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Header/>
+        <div className='page-container'>
+          <Component {...pageProps} />
+        </div>
+      </ChakraProvider>
     </Provider>
   )
 }
