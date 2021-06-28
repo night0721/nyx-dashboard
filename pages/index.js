1c491c927e13b43fa97b13991ee1476dccd57d19
qt-coder <chuchenkhang@gmail.com>
2021-06-28 21:58:43 +0800

    Added CTA and features components

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Card from '../components/Card'
 import { useSession } from 'next-auth/client'
 import { Text } from "@chakra-ui/react"
 import Hero from '../components/Hero/Hero'
+import {CTA} from '../components/CTA'
+import { Features } from '../components/Features/Features'
 
 export default function Home() {
 
@@ -12,6 +14,8 @@ export default function Home() {
   <>
     {!session && <>
       <Hero title='Cath.exe' subtitle='Your favorite CODM bot. Cath.exe is packed to the brim with commands for moderation, stats and of course, CODM.' ctaText='Sign In' image='/logo.png'/>
+      <Features/>
+      <CTA/>
     </>
   }
   {session && <>
