046241284703a525bfc34fa23889f444b77737f9
qt-coder <chuchenkhang@gmail.com>
2021-06-29 09:14:33 +0800

    Added animation to cards

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,29 @@ import Hero from '../components/Hero/Hero'
 import { CTA } from '../components/CTA'
 import { Features } from '../components/Features/Features'
 import {Footer} from '../components/Footer/Footer'
+import {motion} from 'framer-motion'
 
 export default function Home() {
+
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.4,
+        staggerChildren: 0.3
+      }
+    }
+  }
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  }
 
   const [session, loading] = useSession()
 
@@ -22,14 +43,14 @@ export default function Home() {
       }
       {session && <>
         <Text fontSize='4xl'>Welcome {session.user.name}!</Text>
-        <div className='grid'>
-          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' />
-          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' />
-          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' />
-          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' />
-          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' />
-          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' />
-        </div>
+        <motion.div className='grid' variants={container} initial="hidden" animate="visible">
+          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' variants={item}/>
+          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' variants={item}/>
+          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' variants={item}/>
+          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' variants={item}/>
+          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' variants={item}/>
+          <Card title='commands' desc='lorem ipsum dolor sit amet' link='/controlpanel/commands' variants={item}/>
+        </motion.div>
       </>
       }
     </>
