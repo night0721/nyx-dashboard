eb2e85c7f84a7372555ef394265f7ec2e7508bae
NK <oscarfu0721@gmail.com>
2023-03-01 21:03:49 +0000

    fix

--- pages/discord.js
+++ pages/discord.js
@@ -1,6 +1,6 @@
 import {useRouter} from "next/router";
 import {useEffect} from "react";
-export default function discord() {
+export default function Discord() {
   const page = useRouter();
   useEffect(() => {
     page.push("http://discord.gg/SbQHChmGcp");
--- pages/yt.js
+++ pages/yt.js
@@ -1,6 +1,6 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-export default function Github() {
+import {useRouter} from "next/router";
+import {useEffect} from "react";
+export default function Youtube() {
   const page = useRouter();
   useEffect(() => {
     page.push("http://youtube.com/Kirito01");
