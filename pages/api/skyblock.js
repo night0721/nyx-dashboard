ffc864fe96a3c3117a92d8462010c8413117248a
NK <oscarfu0721@gmail.com>
2023-03-01 20:59:47 +0000

    skyblock endpoint

---
+++ pages/api/skyblock.js
@@ -0,0 +1,21 @@
+const {getItemNetworth} = require("skyhelper-networth");
+const nbt = require("prismarine-nbt");
+const parseNbt = require("util").promisify(nbt.parse);
+async function decodeData(buffer) {
+  const parsedNbt = await parseNbt(Buffer.from(buffer, "base64"));
+  return nbt.simplify(parsedNbt);
+}
+
+export default async function handler(req, res) {
+  if (req.method == "GET") {
+    const {ByteData} = JSON.parse(req.body);
+    if (ByteData == undefined)
+      res.status(400).json({error: "ByteData is undefined"});
+    const data = await getItemNetworth((await decodeData(ByteData)).i[0], {
+      cache: true,
+    });
+    res.status(200).json(data);
+  } else {
+    res.status(400).json({error: "This endpoint only accepts GET requests"});
+  }
+}
