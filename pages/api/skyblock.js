e6031126062a5aa4f3bcddb758e8b47db727c161
NK <oscarfu0721@gmail.com>
2023-03-04 17:25:43 +0000

    more error catching, hope it works

--- pages/api/skyblock.js
+++ pages/api/skyblock.js
@@ -5,22 +5,35 @@ async function decodeData(buffer) {
   const parsedNbt = await parseNbt(Buffer.from(buffer, "base64"));
   return nbt.simplify(parsedNbt);
 }
-let dat;
 export default async function handler(req, res) {
   if (req.method == "POST" || req.method == "GET" || req.method == "PUT") {
-    try {
-      dat = JSON.parse(req.body).ByteData;
-    } catch (e) {
-      dat = req.body.ByteData;
+    if (req.body == "") {
+      res.status(400).json({error: "No body was provided"});
+      return;
     }
-    console.log(dat);
-    if (dat == undefined)
-      res.status(400).json({error: "ByteData is undefined"});
-    const data = await getItemNetworth((await decodeData(dat)).i[0], {
-      cache: true,
-    });
-    res.status(200).json(data);
+    console.log(req.body);
+    let dat;
+    try {
+      dat = JSON.parse(req.body).ByteData || req.body.ByteData;
+    } catch (e) {
+      res.status(400).json({error: "ByteData is not a string or undefined"});
+      return;
+    }
+    if (typeof dat !== "string" || typeof dat == "undefined") {
+      res.status(400).json({error: "ByteData is not a string or undefined"});
+      return;
+    }
+    try {
+      const data = await getItemNetworth((await decodeData(dat)).i[0], {
+        cache: true,
+      });
+      res.status(200).json(data);
+    } catch {
+      res.status(400).json({error: "Invalid ByteData"});
+    }
   } else {
-    res.status(400).json({error: "This endpoint only accepts GET requests"});
+    res
+      .status(400)
+      .json({error: "This endpoint only accepts GET, POST and PUT requests"});
   }
 }
